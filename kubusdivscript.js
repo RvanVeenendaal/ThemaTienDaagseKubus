74f92d4bf7a154bca054732a9efd6f3600f836d1
Remco van Veenendaal <remco.van.veenendaal@nationaalarchief.nl>
2024-03-10 16:44:54 +0100

    Update kubusdivscript.js
    Update n.a.v. nieuwste versie.

--- kubusdivscript.js
+++ kubusdivscript.js
@@ -1,18 +1,57 @@
 let isDragging = false;
 let lastMouseX;
 let lastMouseY;
-let currentRotationX = -20;
-let currentRotationY = -20;
+let currentRotationX = -10;
+let currentRotationY = -10;
 let faceWidthAndHeight = 300;
 const isOverflown = ({ clientWidth, scrollWidth }) => scrollWidth > clientWidth
-
 let cube = document.querySelector(".cube");
-cube.style.transform = 
-`rotateX(${currentRotationX}deg)
-rotateY(${currentRotationY}deg)`;
 cube.addEventListener("mousedown", onMouseDown);
 cube.addEventListener("mouseup", onMouseUp);
 cube.addEventListener("mousemove", onMouseMove);
+let links = document.querySelector(".draaiLinks");
+links.addEventListener("click", draaiLinks);
+let rechts = document.querySelector(".draaiRechts");
+rechts.addEventListener("click", draaiRechts);
+let omhoog = document.querySelector(".draaiOmhoog");
+omhoog.addEventListener("click", draaiOmhoog);
+let omlaag = document.querySelector(".draaiOmlaag");
+omlaag.addEventListener("click", draaiOmlaag);
+let handleiding = document.querySelector(".handleiding");
+handleiding.addEventListener("click", toggleHandleiding);
+
+function toggleHandleiding() {
+  var toggle = document.querySelector(".toggle");
+  if (toggle.style.display === "none") {
+    toggle.style.display = "block";
+    handleiding.innerHTML = "[verberg]";
+  } else {
+    toggle.style.display = "none";
+    handleiding.innerHTML = "[toon]";
+  }
+} 
+
+function draaiLinks(){
+    draai(0, -90);
+}
+
+function draaiRechts(){
+    draai(0, 90);
+}
+
+function draaiOmhoog(){
+    draai(90, 0);
+}
+
+function draaiOmlaag(){
+    draai(-90, 0);
+}
+
+function draai(x, y){
+    currentRotationX += x;
+    currentRotationY += y;
+    updateCubeRotation();
+}
 
 function onMouseDown(event) {
     isDragging = true;
@@ -41,4 +80,28 @@ function updateCubeRotation() {
     cube.style.transform = 
     `rotateX(${currentRotationX}deg)
     rotateY(${currentRotationY}deg)`;
+}
+
+function setInitialPerspective(){
+    updateCubeRotation();
+    switch(Math.floor(Math.random() * 6)) {
+        case 1:
+            draaiLinks();
+            break;
+        case 2:
+            draaiRechts();
+            break;
+        case 3:
+            draaiOmhoog();
+            break;
+        case 4:
+            draaiOmlaag();
+            break;
+        case 5:
+            draaiLinks();
+            draaiLinks();
+            break;
+        default:
+            // do nothing
+    } 
 }
